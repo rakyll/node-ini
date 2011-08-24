9d63cd19a6586c254a3dfa22c8528ddeba88a4b4
Kevin Le Brun <lebrun.k@gmail.com>
2011-08-24 12:10:25 +0200

    value remains a string and is trimmed

--- test/file.js
+++ test/file.js
@@ -1,29 +1,30 @@
 module.exports = {
     "default": {
-        "foo": "bar "
+        "foo": "bar",
+        "test": "'123'"
     },
     "section": {
-        "foo": "bar ; another comment",
+        "foo": "bar ; wrong comment",
         "error": {
-            "404": "page not found",
-            "500": "\"application error\""
+            "404": "\\\"page not found\\\"",
+            "500": "application error"
         }
     },
     "test": {
         "foo": "baz",
         "error": {
-            "404": "page not found",
-            "500": "\"application error\""
+            "404": "\\\"page not found\\\"",
+            "500": "application error"
         },
         "arr": [23, 42, 55]
     },
     "third": {
         "foo": "baz",
         "error": {
-            "404": "page not found",
-            "500": "\"application error\""
+            "404": "\\\"page not found\\\"",
+            "500": "application error"
         },
         "arr": [23, 42, 55],
-        "toto": "tata"
+        "toto": "\"tata\""
     }
 };
